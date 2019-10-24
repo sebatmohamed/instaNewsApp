b2fc4c693af7ef75cbb63a23652e84832c1d88c7
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-24 17:35:53 -0400

    chore: added gulpfile javascript

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+
+const terser = require("gulp-terser"),
+  rename = require("gulp-rename"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cssnano = require("gulp-cssnano"),
+  sourcemaps = require("gulp-sourcemaps"),
+  eslint = require("gulp-eslint"),
+  browserSync = require("browser-sync");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss", {sourcemaps: true})
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemaps.write("../maps"))
+    .pipe(gulp.dest("./build/css"));
+});
+
+gulp.task("default", function() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(terser())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest("./build/js"));
+});
+
+// gulp.task("default", function(done) {
+//   console.log("hello it is I, Gulp");
+//   done();
+// });
--- js/main.js
+++ js/main.js
@@ -7,27 +7,12 @@ $(function() {
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
       ).done(function(data) {
         $.each(data.results, function(index, value) {
-          const $media = $(value.multimedia[4]);
-
-          const $mediaArray = $.map($media[0], function(value, index) {
-            return [value];
-          });
-
-          if (typeof $mediaArray[0] === "undefined") {
-            JSON.parse(JSON.stringify($mediaArray));
-            return
-          }
-
-          console.log($mediaArray[0]);
-          const imageUrl = $mediaArray[0]
-
-        
-          // try to append images as background in css
-          $("#top-stories").append(` <img class="image" src="${$mediaArray[0]}"/>`);
-
-          // $("#top-stories").after('<div></div>').css('background-image', 'url("' + imageUrl + '")');
-
-          return index < 11;
+          const $media = value.multimedia;
+          
+          if($media.length !== 0){
+            $("#top-stories").append(` <img class="image" src="${$media[4].url}"/>`)
+            return index < 11
+          } 
         });
       });
     });
