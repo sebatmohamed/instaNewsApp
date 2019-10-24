58986367530b44f6956acbf579a29a4cbb529d38
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-24 00:19:56 -0400

    fix: removed undefined propety to append images

--- js/main.js
+++ js/main.js
@@ -5,11 +5,26 @@ $(function() {
 
       $.getJSON(
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
-      )
-      .done(function(data) {
+      ).done(function(data) {
         $.each(data.results, function(index, value) {
-          console.log(value.title)
-          return index < 11
+          const $media = $(value.multimedia[2]);
+
+          const $mediaArray = $.map($media[0], function(value, index) {
+            return [value];
+          });
+
+          if (typeof $mediaArray[0] === "undefined") {
+            JSON.parse(JSON.stringify($mediaArray));
+            return
+          }
+
+          console.log($mediaArray[0]);
+
+          $("#top-stories").append(` <div>
+                                      <img class="image" src="${$mediaArray[0]}"/>
+                                     </div>`);
+
+          return index < 11;
         });
       });
     });
