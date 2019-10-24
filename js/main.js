fd89d202ed211214529c94e9d8103b5989830bd1
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-24 09:44:16 -0400

    style: appended image as background into CSS but not HTML

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function() {
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
       ).done(function(data) {
         $.each(data.results, function(index, value) {
-          const $media = $(value.multimedia[2]);
+          const $media = $(value.multimedia[4]);
 
           const $mediaArray = $.map($media[0], function(value, index) {
             return [value];
@@ -19,10 +19,13 @@ $(function() {
           }
 
           console.log($mediaArray[0]);
+          const imageUrl = $mediaArray[0]
 
-          $("#top-stories").append(` <div>
-                                      <img class="image" src="${$mediaArray[0]}"/>
-                                     </div>`);
+        
+          // try to append images as background in css
+          // $("#top-stories").append(` <img class="image" src="${$mediaArray[0]}"/>`);
+
+          $("#top-stories").after('<div></div>').css('background-image', 'url("' + imageUrl + '")');
 
           return index < 11;
         });
