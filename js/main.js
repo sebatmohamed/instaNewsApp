4603930ff20e5901579ee235193916e54158c76b
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-25 17:51:28 -0400

    fix: removed previously appended top stories when new section is clicked

--- js/main.js
+++ js/main.js
@@ -3,25 +3,30 @@ $(function() {
     $("#my-select-menu").on("change", function() {
       const section = $("#my-select-menu").val();
 
+      let counter = 0
+
       $.getJSON(
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
       ).done(function(data) {
+
         $.each(data.results, function(index, value) {
           const $media = value.multimedia;
           const $abstract = value.abstract;
 
-          if ($media.length !== 0) {
+          if ($media.length !== 0 && counter >= 0) {
+            counter++;
             $("#top-stories").append(` <figure class="stories">
                                         <img class="image" src="${$media[4].url}"/>
                                         <figcaption class="abstract">${$abstract}</figcaption>
                                       </figure>`);
-            return index < 12;
+            return counter < 12;
+
           }
         });
-
-        
-
       });
+
+      $("#top-stories").html("").attr("src", `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`)
+
     });
   });
 });
