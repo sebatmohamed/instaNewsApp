dd20d1f6c50190ce2139f4a29793c89d27da73fd
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-27 16:08:26 -0400

    feat: added loading gif before images displayed

--- js/main.js
+++ js/main.js
@@ -1,46 +1,42 @@
 $(function() {
   $(function() {
     $("#sections").on("change", function() {
-      
-      $("#top-stories").empty();
+      $(".loader-gif").css("display", "block");
 
-      $(".loader-gif").show();
+      $("#top-stories").empty();
 
       const section = $("#sections").val();
 
-      let counter = 0
+      let counter = 0;
 
       $.getJSON(
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
       )
-      
-      .done(function(data) {
 
-        $.each(data.results, function(index, value) {
-          const $media = value.multimedia;
-          const $abstract = value.abstract;
-          const $articlelink = value.url
+        .done(function(data) {
+          $(".loader-gif").hide();
 
-          if ($media.length !== 0 && counter >= 0) {
-            counter++;
-            $("#top-stories").append(` <figure class="stories">
+          $.each(data.results, function(index, value) {
+            const $media = value.multimedia;
+            const $abstract = value.abstract;
+            const $articlelink = value.url;
+
+            if ($media.length !== 0 && counter >= 0) {
+              counter++;
+              $("#top-stories").append(` <figure class="stories">
                                         <a target="_blank" href="${$articlelink}">
                                         <img class="image" src="${$media[4].url}"/></a>
                                         <figcaption class="abstract">${$abstract}</figcaption>
                                       </figure>`);
-            return counter < 12;
-          }
+              return counter < 12;
+            }
+          });
+        })
+        .fail(function(data) {
+          $("#top-stories").append(
+            "<p>Sorry there was a problem, please try again</p>"
+          );
         });
-      })
-
-      .fail(function(data) {
-        $("#top-stories").append('<p>Sorry there was a problem, please try again</p>')
-      })
-
-      .always(function(data) {
-        $(".loader.gif").hide()
-      })
-      
     });
   });
 });
