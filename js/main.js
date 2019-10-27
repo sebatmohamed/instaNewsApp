49cd4709d2aba843129bfdf7f3c3de1c1fd207ee
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-10-27 12:36:35 -0400

    feat: added a fail functionality if API is not fetching data

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(function() {
   $(function() {
-    $("#my-select-menu").on("change", function() {
+    $("#sections").on("change", function() {
       
-      const section = $("#my-select-menu").val();
+      const section = $("#sections").val();
 
       let counter = 0
 
-
       $.getJSON(
         `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`
-      ).done(function(data) {
+      )
+      
+      .done(function(data) {
 
         $.each(data.results, function(index, value) {
           const $media = value.multimedia;
@@ -21,16 +22,21 @@ $(function() {
             $("#top-stories").append(` <figure class="stories">
                                         <a target="_blank" href="${$articlelink}">
                                         <img class="image" src="${$media[4].url}"/></a>
+                                        <img src="/assets/images/ajax-loader.gif" alt="Loading..." class="loader-gif"/>
                                         <figcaption class="abstract">${$abstract}</figcaption>
                                       </figure>`);
             return counter < 12;
-
           }
         });
-      });
+      })
+
+      .fail(function(data) {
+        $("#top-stories").append('<p>Sorry there was a problem, please try again</p>')
+      })
 
       $("#top-stories").html("").attr("src", `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=GcbW6tsU7sr1adidcqfTOzJOuNt1kwBv`)
 
+      
     });
   });
 });
